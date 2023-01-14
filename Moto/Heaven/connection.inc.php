9d69e610b5df61d991c25267a2dd2211ee5f86e4
Karthik R Shetty <118684341+85karthikrs@users.noreply.github.com>
2023-01-14 22:54:28 +0530

    Create connection.inc.php

---
+++ Moto/Heaven/connection.inc.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+date_default_timezone_set("Asia/kolkata");
+$con=mysqli_connect("localhost","username","password","db_name");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Moto/Heaven/');
+define('SITE_PATH','http://127.0.0.1/Moto/Heaven/');
+
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+
+define('PRODUCT_MULTIPLE_IMAGE_SERVER_PATH',SERVER_PATH.'media/product_images/');
+define('PRODUCT_MULTIPLE_IMAGE_SITE_PATH',SITE_PATH.'media/product_images/');
+?>
